c3ba0f993e4252458b8d4c28b950caa58c2f5fef
Shobeyr Moshashaee <computer.engineer71@gmail.com>
2025-05-11 23:40:51 +0330

    fix homepage error

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.http import HttpResponse
 from .views import GroupListCreate, ServerListCreate
 
+def home(request):
+    return HttpResponse("<h1>Welcome to Abalon IP Management</h1>")
+
 urlpatterns = [
+    path('', home, name='home'),
     path('api/groups/', GroupListCreate.as_view(), name='group-list'),
     path('api/servers/', ServerListCreate.as_view(), name='server-list'),
 ]
